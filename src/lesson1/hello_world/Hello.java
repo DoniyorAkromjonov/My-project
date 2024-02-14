e7984fcb1d6adf11e7d37c3d92534af3ff01911a
DoniyorAkromjonov <akromjonovdoniyor13@gmail.com>
2024-02-14 11:19:24 +0500

    Yangi fayl ochdim

--- src/lesson1/hello_world/Hello.java
+++ src/lesson1/hello_world/Hello.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("lesson1.hello_world.Hello World!");
+        System.out.print("lesson1.hello_world.Hello World!");
 
 
     }
